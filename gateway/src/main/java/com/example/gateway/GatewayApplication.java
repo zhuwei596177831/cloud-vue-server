c0dd41d6e5c1b56a3c060b67cf72b35ceb2b8cf3
朱伟伟 <596177831@qq.com>
2021-08-05 23:53:00 +0800

    GatewayApplication

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,11 +1,6 @@
 package com.example.gateway;
 
-import com.example.core.entity.Result;
-import com.example.system.entity.User;
-import com.example.system.openFeign.UserFeign;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
@@ -18,18 +13,17 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
 import java.util.*;
 
 @SpringBootApplication(scanBasePackages = {"com.example"})
 @EnableFeignClients(basePackages = {
         "com.example.system.openFeign"
 })
-@RestController
 public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
     /**
      * @author: 朱伟伟
@@ -56,23 +50,6 @@ public class GatewayApplication {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance);
-    }
-
-    @GetMapping("/test")
-    public Result test() {
-        User user = new User();
-        user.setId(Long.MAX_VALUE);
-        user.setName(Long.MAX_VALUE + "");
-        user.setInputTime(LocalDateTime.now());
-        Map<String, Object> map = new HashMap<>();
-        map.put("user", user);
-        map.put("date", new Date());
-        return Result.ok(map);
-    }
-
-
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
