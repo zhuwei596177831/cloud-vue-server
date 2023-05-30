575f9946071b319b01f17750eaf12e2d2c01b4c9
朱伟伟 <596177831@qq.com>
2023-05-30 14:39:27 +0800

    多级缓存代码优化

--- common/core-web/src/main/java/com/example/coreweb/cache/customizers/TransactionAwareCacheManagerCustomizer.java
+++ common/core-web/src/main/java/com/example/coreweb/cache/customizers/TransactionAwareCacheManagerCustomizer.java
@@ -2,17 +2,20 @@ package com.example.coreweb.cache.customizers;
 
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.cache.transaction.AbstractTransactionSupportingCacheManager;
-import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 /**
- * 设置该CacheManager是否应该公开事务感知缓存对象
+ * 设置CacheManager是否应该公开事务感知缓存对象
  * 默认为false，将其设置为“true”，将缓存操作与正在进行的spring管理的事务同步，
  * 仅在事务提交(afterCommit)后执行实际的put、evict、clear操作
+ * <p/>
+ * 修正：配了也没用，执行到put操作时，事务管理器已将当前线程的事务清理掉
  *
  * @author 朱伟伟
  * @date 2023-05-30 14:10:58
+ * @see TransactionSynchronizationManager#clearSynchronization()
  */
-@Component
+//@Component
 public class TransactionAwareCacheManagerCustomizer implements CacheManagerCustomizer<AbstractTransactionSupportingCacheManager> {
 
     @Override
