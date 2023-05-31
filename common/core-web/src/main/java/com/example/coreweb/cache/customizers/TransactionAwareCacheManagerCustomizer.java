d5a8ee616a055004339ef284c557475d64cf91c9
朱伟伟 <596177831@qq.com>
2023-05-31 09:50:15 +0800

    缓存代码优化

--- common/core-web/src/main/java/com/example/coreweb/cache/customizers/TransactionAwareCacheManagerCustomizer.java
+++ common/core-web/src/main/java/com/example/coreweb/cache/customizers/TransactionAwareCacheManagerCustomizer.java
@@ -9,7 +9,9 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
  * 默认为false，将其设置为“true”，将缓存操作与正在进行的spring管理的事务同步，
  * 仅在事务提交(afterCommit)后执行实际的put、evict、clear操作
  * <p/>
- * 修正：配了也没用，执行到put操作时，事务管理器已将当前线程的事务清理掉
+ * 说明：默认情况下，不需要配置，默认缓存的MethodInterceptor先于事务执行，到put操作时，事务管理器已将当前线程的事务清理掉
+ * <p/>
+ * 当手动修改了事务advice的优先级，先于缓存执行时，需要配置当前bean
  *
  * @author 朱伟伟
  * @date 2023-05-30 14:10:58
